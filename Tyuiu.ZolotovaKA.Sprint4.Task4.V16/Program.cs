341c310840a3829bd86805dddc90b9a7c75a3e2d
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:51:39 +0500

    Task4

---
+++ Tyuiu.ZolotovaKA.Sprint4.Task4.V16/Program.cs
@@ -0,0 +1,77 @@
+﻿using Tyuiu.ZolotovaKA.Sprint4.Task4.V16.Lib;
+static void Main(string[] args)
+{
+    DataService ds = new DataService();
+    Random rnd = new Random();
+
+    Console.Title = "Спринт #4 |Выполнила: Золотова К.А | ИСПб-24-1    ";
+
+    Console.WriteLine("**");
+    Console.WriteLine(" Спринт #4                                                               ");
+    Console.WriteLine(" Тема: Обработка структурных типов                                        ");
+    Console.WriteLine(" Задание #4                                                             ");
+    Console.WriteLine(" Вариант #16                                                        ");
+    Console.WriteLine(" Выполнила: Золотова К.А | ИСПб-24-1                      ");
+    Console.WriteLine("**");
+    Console.WriteLine("* УСЛОВИЕ:                                                                ");
+    Console.WriteLine(" Дан двумерный целочисленный массив 5 на 5 элементов, заполненный значениями с клавиатуры в диапазоне от 3 до 7.  ");
+    Console.WriteLine(" Заменить четные элементы массива на 1.    ");
+    Console.WriteLine("**");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        ");
+    Console.WriteLine("**");
+
+    Console.Write("Введите количество строк = ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов = ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("Введите элемент[{0},{1}] => ", i + 1, j + 1);
+            array[i, j] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Массив:");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("|  " + array[i, j] + "  |  ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    int[,] resArray = ds.Calculate(array);
+
+    Console.WriteLine("Ответ:");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("|  " + resArray[i, j] + "  |  ");
+        }
+        Console.WriteLine();
+    }
+    Console.ReadKey();
+}
+    }
+}
+
+        
+
+
+    
