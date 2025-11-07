65b986af8a02320e336ca96ecdcc77db5ccb8ce0
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:54:34 +0500

    Task5

---
+++ Tyuiu.ZolotovaKA.Sprint4.Task5.V8.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ZolotovaKA.Sprint4.Task5.V8.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint4.Task5.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+
+            int[,] mas4 = new int[5, 5]
+             { { 2, -1, 3, -2, 4 },
+             { 0, -3, 1, 2, -1 },
+             { 3, 4, -2, 0, 1 },
+             { -1, 2, 3, -3, 4 },
+             { 2, 0, -1, 3, -2 } };
+
+
+            int[,] res = ds.Calculate(mas4);
+
+
+            int[,] wait = new int[5, 5]
+            { { 2, 0, 3, 0, 4 },
+            { 0, 0, 1, 2, 0 },
+            { 3, 4, 0, 0, 1 },
+            { 0, 2, 3, 0, 4 },
+            { 2, 0, 0, 3, 0 } };
+
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZolotovaKA.Sprint4.Task5.V8/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.ZolotovaKA.Sprint4.Task5.V8.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #4| Выполнила: Золотова К. А. | ИCПБ-24-1";
+Console.WriteLine("****************************************************");
+Console.WriteLine("* Спринт #4                                        *");
+Console.WriteLine("*Тема:Двумерные массивы(Генератор случайных чисел) *");
+Console.WriteLine("* Задание #5                                       *");
+Console.WriteLine("* Вариант #8                                       *");
+Console.WriteLine("* Выполнила: Золотова К. А. | ИCПБ-24-1            *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                         *");
+Console.WriteLine("*Дан двумерный целочисленный массив 5 на5 элементов*");
+Console.WriteLine("*заполненный случайными значениями   в диапазоне от*");
+Console.WriteLine("*от -3 до 4. Заменить отрицательные элементы на 0. *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("*                                                  *");
+Console.WriteLine("****************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                 *");
+Console.WriteLine("****************************************************");
+Random rnd = new Random();
+Console.WriteLine("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество Столбцов в массиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] mtrx = new int[rows, columns];
+Console.WriteLine("****************************************************");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        mtrx[i, j] = rnd.Next(-3, 4);
+    }
+}
+
+Console.WriteLine("\nМассив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("****************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                       *");
+Console.WriteLine("****************************************************");
+int[,] res = ds.Calculate(mtrx);
+
+Console.WriteLine("Количество нулевых элементов = " + res);
+Console.ReadKey();
