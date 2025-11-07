eb2d21a3299c29898f1fe24fc0ba8ddcf280fc93
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:39:39 +0500

    Task0

---
+++ Tyuiu.ZolotovaKA.Sprint4.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ZolotovaKA.Sprint4.Task0.V15.Lib
+{
+    public class DataService : ISprint4Task0V15
+    {
+        public int GetMultOddArrEl(int[] array)
+        {
+            int mult = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    mult *= array[i];
+                }
+            }
+            return mult;
+        }
+    }
+}
---
+++ Tyuiu.ZolotovaKA.Sprint4.Task0.V15/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ZolotovaKA.Sprint4.Task0.V15.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт 4 | выполнила: Золотов К.А. | ИСПБ-24-1";
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("Спринт №4                                                                       ");
+        Console.WriteLine("Тема: Одномерные массивы (статический ввод)                                     ");
+        Console.WriteLine("**********************************************                                  ");
+        Console.WriteLine("Задание №0                                                                      ");
+        Console.WriteLine("Вариант №15                                                                     ");
+        Console.WriteLine("выполнила: Золотов К.А. | ИСПБ-24-1                                             ");
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ :                                                               ");
+        Console.WriteLine("********************************************************************************");
+
+        Console.WriteLine("исходный массив");
+        int[] array = { 9, 8, 7, 6, 5, 7, 3, 2, 7, 3 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("Результат");
+        Console.WriteLine("********************************************************************************");
+
+        int result = ds.GetMultOddArrEl(array);
+        Console.WriteLine("Произведение нечётных элементов = " + result);
+
+        Console.ReadKey();
+    }
+}
+}
+       
