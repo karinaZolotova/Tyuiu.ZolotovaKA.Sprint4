0f9534d81caddf2359b523a5140ec9ad7a8cfd33
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:57:32 +0500

    Task6

---
+++ Tyuiu.ZolotovaKA.Sprint4.Task6.V3.Test/DataSerivceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ZolotovaKA.Sprint4.Task6.V3.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint4.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var month = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", };
+            int res = ds.Calculate(month);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
